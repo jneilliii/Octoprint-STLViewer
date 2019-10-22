c1608fb9a041dc52eae3c98b440f0be9cbaaf492
jneilliii <jneilliii+github@gmail.com>
2019-10-21 22:49:32 -0400

    0.4.2
    - Added stl upload support in case Cura legacy plugin is not installed
    - Added Python 3 compatibility

--- octoprint_stlviewer/__init__.py
+++ octoprint_stlviewer/__init__.py
@@ -18,5 +18,42 @@ class stlviewer(octoprint.plugin.StartupPlugin,
 			js=["js/stlviewer.js","js/jsc3d.js","js/jsc3d.touch.js"]
 		)
 
+	##-- Image upload extenstion tree hook
+	def get_extension_tree(self, *args, **kwargs):
+		return dict(
+			model=dict(
+				stlviewer=["stl"]
+			)
+		)
+
+	##~~ Softwareupdate hook
+	def get_update_information(self):
+		return dict(
+			stlviewer=dict(
+				displayName="STL Viewer",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-STLViewer",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-STLViewer/archive/{target_version}.zip"
+			)
+		)
+
 __plugin_name__ = "STL Viewer"
-__plugin_implementation__ = stlviewer()
+__plugin_pythoncompat__ = ">=2.7,<4"
+
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = stlviewer()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+		"octoprint.filemanager.extension_tree": __plugin_implementation__.get_extension_tree
+	}
